13e96083aaabcc57842cb0f51e0cd71f4d3ac9dd
Hasan <hasan.doctype@gmail.com>
2021-06-07 20:28:53 +0400

    add js lesson1

---
+++ JS/VanillaJS/Lesson1/main.js
@@ -0,0 +1,21 @@
+
+// document.write("Hello JS");
+// document.writeln("Hello JS");
+
+console.log("Hi JS");
+
+// document.write("Tets1<br>");
+// debugger;
+// document.write("Tets2<br>");
+// debugger;
+// document.wrie("Tets3<br>");
+// debugger;
+// document.write("Tets4<br>");
+// debugger;
+// document.write("Tets5<br>");
+// debugger;
+// document.write("Tets6<br>");
+// debugger;
+// document.write("Tets7<br>");
+// debugger;
+
