c09a8c0572460bddb8343d24cd7068899c28d930
Hasan <hasan.doctype@gmail.com>
2021-06-09 17:07:21 +0400

    add js lesson2

---
+++ JS/VanillaJS/Lesson2/main.js
@@ -0,0 +1,38 @@
+// All Data Type 
+// --------------------------------------------------------------------------------------------------
+// Primitive (Bəsit)  Data(Data) Type(Tipi) Start
+var infomationText1   = "Salam1";     //String(mətn)      data type ("Hi")
+var infomationText2   = 'Salam2';     //String(mətn)      data type ("Hi")
+var infomationNumb    = 5 ;           //Number(rəqəm)     data type (5)
+var infomationBoolean = true ;        //Boolean(məntiqi)  data type (true/flase)
+
+document.write(infomationText1     + "<br>");
+document.write(infomationText2     + "<br>");
+document.write(infomationNumb      + "10" + "<br>");
+document.write(infomationBoolean   + "<br>");
+// Basic(Bəsit)        Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Composite(Birləşik) Data(Data) Type(Tipi) Start
+var infomationArray  =  ['pc',5,true,'table'];    //Array
+var infomationObject =  {
+    name:"Samsung",
+    ROM: "OneUI"
+};                                                //Object
+
+document.write(infomationArray          + "<br>");
+document.write(infomationObject.name    + "<br>");
+document.write(infomationObject.ROM     + "<br>");
+// Composite(Birləşik) Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------
+// Special(Xüsusi)     Data(Data) Type(Tipi) Start
+var informationUndefined = "";   //Undefined
+var informationNULL      = null; //Null
+
+document.write(informationUndefined          + "<br>");
+document.write(informationNULL               + "<br>");
+// Special(Xüsusi)     Data(Data) Type(Tipi) End
+// --------------------------------------------------------------------------------------------------
