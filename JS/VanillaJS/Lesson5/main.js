7c0784bdb95b30ed57bbc09a0d32a7afa67f82fa
Hasan <hasan.doctype@gmail.com>
2021-06-18 16:57:48 +0400

    add loop

---
+++ JS/VanillaJS/Lesson5/main.js
@@ -0,0 +1,18 @@
+// var infoData = 1;
+
+
+// while(infoData <= 50){
+//     document.write(infoData +"<br>");
+//     infoData++;
+// }
+
+
+// var c = 100;
+// do{
+//     document.write(infoData +"<br>");
+//     infoData++;
+// }while(infoData <= 50);
+
+// for(var infoData = 100; infoData <= 200; infoData++){
+//     document.write(infoData+"<br>");
+// } 
