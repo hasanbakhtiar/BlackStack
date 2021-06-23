e53f20a7dac89a5d8d6a997934b494ba6371e2d8
Hasan <hasan.doctype@gmail.com>
2021-06-23 17:08:05 +0400

    add js lesson6

--- JS/VanillaJS/Lesson5/main.js
+++ JS/VanillaJS/Lesson5/main.js
@@ -16,3 +16,67 @@
 // for(var infoData = 100; infoData <= 200; infoData++){
 //     document.write(infoData+"<br>");
 // } 
+
+// 1.3.for in
+// var misal = ["JS" , "PHP", "C"];
+
+// for(netice in misal){
+//   document.write(misal[netice]+"<br>");
+// }
+// var misal = ["JS", " PHP", " C"];
+
+//  var netice;
+//  for(netice in misal){
+//          document.write(misal[netice]);
+//  }
+
+// var infoArray = ["Python ", "GoLang ", "Java "];
+
+// for(result in infoArray){
+//     document.write(infoArray[result]);
+// }
+
+
+// 1.4. break və continue ifadəsi
+
+// for (var index = 1; index <= 10; index++) {
+
+//     if(index == 5){
+//         break;
+// }
+//         document.write(index + "<br>");
+       
+        
+// }
+// var i = 1;
+// while(i<10){
+//         if (i == 5) {
+//                 break;
+                
+//         }
+//         document.write(i+ "<br>");
+//         i++;
+// }
+
+
+// for (var index = 1; index <= 10; index++) {
+        
+
+//         if (index==4) {
+//                 continue;
+//         }
+
+//         document.write(index + "<br>");
+        
+// }
+// var i = 1;
+
+// while (i<10) {
+//         if (i == 4) {
+//                 continue;
+//         }
+//         document.write(i + "<br>");
+//         i++;
+       
+      
+// }
