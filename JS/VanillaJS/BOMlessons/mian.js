fd2d424a566bbe207eeb1c8168d74843aeedffe2
Hasan <hasan.doctype@gmail.com>
2021-07-14 15:02:08 +0400

    add BOM lesson

---
+++ JS/VanillaJS/BOMlessons/mian.js
@@ -0,0 +1,15 @@
+
+// Window 
+// https://www.w3schools.com/js/js_window.asp
+// Screen
+// https://www.w3schools.com/js/js_window_screen.asp
+// Location
+// https://www.w3schools.com/js/js_window_location.asp
+// History
+// https://www.w3schools.com/js/js_window_history.asp
+// Navigator
+// https://www.w3schools.com/js/js_window_navigator.asp
+// Popup
+// https://www.w3schools.com/js/js_popup.asp
+// Timing
+// https://www.w3schools.com/js/js_timing.asp
