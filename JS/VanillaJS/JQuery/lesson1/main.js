20250c00803681f2a67900b622698894a2d76734
Hasan <hasan.doctype@gmail.com>
2021-07-16 17:18:06 +0400

    add new file

---
+++ JS/VanillaJS/JQuery/lesson1/main.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//    $('h1').click(function(){
+//        $('h1').hide();
+//    }) 
+// });
+
+$('button').click(function(){
+    $('.box').hide();
+})
+
+
+// Nə?
+   // Necə?
+        // Nəyi?
+           // Etsin? 
+
+
+
+// $(function(){
+    
+// $("h1"/*nə ilə tətbiq edən*/)/*nə zaman tətbiq edən*/.click(function(){
+//   $(this/*tetbiq olunan*/).hide()/*tetbiq eden hereket*/;
+// });
+// });
---
+++ JS/VanillaJS/JQuery/lesson2/main.js
@@ -0,0 +1,16 @@
+// JQ All Selector
+// https://www.w3schools.com/jquery/jquery_selectors.asp
+// Events
+// https://www.w3schools.com/jquery/jquery_events.asp
+// Fade
+// https://www.w3schools.com/jquery/jquery_fade.asp
+// Slide
+// https://www.w3schools.com/jquery/jquery_slide.asp
+// Animate
+// https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_animation
+// Stop
+// https://www.w3schools.com/jquery/jquery_stop.asp
+// CallBack 
+// https://www.w3schools.com/jquery/jquery_callback.asp
+// Chaining
+// https://www.w3schools.com/jquery/jquery_chaining.asp
