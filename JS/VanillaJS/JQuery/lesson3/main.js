7bf4d788adacdd626a1c8f3a99c117fd3eea340f
Hasan <hasan.doctype@gmail.com>
2021-07-17 14:36:51 +0400

    add JQuery Lesson3

---
+++ JS/VanillaJS/JQuery/lesson3/main.js
@@ -0,0 +1,24 @@
+// JQ GET
+// https://www.w3schools.com/jquery/jquery_dom_get.asp
+// JQ SET
+// https://www.w3schools.com/jquery/jquery_dom_set.asp
+// JQ ADD
+// https://www.w3schools.com/jquery/jquery_dom_add.asp
+// JQ Remove
+// https://www.w3schools.com/jquery/jquery_dom_remove.asp
+// JQ CSS Class 
+// https://www.w3schools.com/jquery/jquery_css_classes.asp
+// JQ css()
+// https://www.w3schools.com/jquery/jquery_css.asp
+// JQ Dimensions
+// https://www.w3schools.com/jquery/jquery_dimensions.asp
+// JQ Traversing
+// https://www.w3schools.com/jquery/jquery_traversing.asp
+// JQ Ancestors
+// https://www.w3schools.com/jquery/jquery_traversing_ancestors.asp
+// JQ Descendants
+// https://www.w3schools.com/jquery/jquery_traversing_descendants.asp
+// JQ Siblings
+// https://www.w3schools.com/jquery/jquery_traversing_siblings.asp
+// JQ Filtering
+// https://www.w3schools.com/jquery/jquery_traversing_filtering.asp
