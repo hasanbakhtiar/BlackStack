604aa6e3c8c60994755293c45d30fb26016f8c38
Hasan <hasan.doctype@gmail.com>
2021-07-30 17:41:58 +0400

    add react lesson1

---
+++ JS/ReactJS/Lesson1/public/scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var root = document.getElementById('root');
+var temp = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
+  style: {
+    "color": "red",
+    "margin-left": "100px"
+  }
+}, "hi"), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum officiis ea id nobis amet cumque temporibus odio, excepturi repellendus voluptate accusamus porro, quasi tempora tempore dicta! Maiores, dolores vitae."));
+ReactDOM.render(temp, root);
---
+++ JS/ReactJS/Lesson1/src/app.js
@@ -0,0 +1,13 @@
+var root = document.getElementById('root');
+
+
+
+var temp = 
+(
+    <div>
+        <h1 style={{"color":"red","margin-left":"100px"}}>hi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam laborum officiis ea id nobis amet cumque temporibus odio, excepturi repellendus voluptate accusamus porro, quasi tempora tempore dicta! Maiores, dolores vitae.</p>
+    </div>
+)
+
+ReactDOM.render(temp,root)
