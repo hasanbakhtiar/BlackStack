d934b2ce2f3fcf54c06568cc76e50b85afd11bea
Hasan <hasan.doctype@gmail.com>
2021-08-04 13:22:31 +0400

    add webpack

---
+++ JS/ReactJS/Lesson2/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import  '../node_modules/bootstrap/scss/bootstrap.scss';
+import  './sass/style.scss';
+
+
+
+
+var root = document.getElementById("root");
+
+var temp = (
+    <div className="container">
+    <h1 className="btn btn-success" >hello</h1>
+
+    </div>
+)
+
+ReactDOM.render(temp,root);
